bf119b6f07e4f0a38f4d132c284762f1efd857ca
Rosux <jcsbt2015@gmail.com>
2023-11-25 14:05:42 +0100

    simple rectangle (with rotation) pyhsics check

--- src/Collision.py
+++ src/Collision.py
@@ -1,67 +1,71 @@
-from math import sqrt
+from vector2d import Vector2D
+from math import sqrt, cos, sin, radians
+import sys
 
 class RectangleCollision:
-    def __init__(self, position=(0, 0), width=0, height=0, rotation=0):
+    def __init__(self, position=Vector2D(0, 0), width=0, height=0, rotation=0):
         self.position = position
         self.width = width
         self.height = height
         self.rotation = rotation
 
-    def normalize(self, vector):
-        norm = sqrt(vector[0] ** 2 + vector[1] ** 2)
-        return vector[0] / norm, vector[1] / norm
+    def Normalize(self, vector):
+        norm = sqrt(vector.x ** 2 + vector.y ** 2)
+        if norm == 0:
+            return Vector2D(0, 0)
+        return Vector2D(vector.x / norm, vector.y / norm)
 
-    def dot(self, vector1, vector2):
-        return vector1[0] * vector2[0] + vector1[1] * vector2[1]
-
-    def edge_direction(self, point0, point1):
-        return point1[0] - point0[0], point1[1] - point0[1]
-
-    def orthogonal(self, vector):
-        return vector[1], -vector[0]
-
-    def vertices_to_edges(self):
-        half_width = self.width / 2
-        half_height = self.height / 2
-        vertices = [
-            (-half_width, -half_height),
-            (half_width, -half_height),
-            (half_width, half_height),
-            (-half_width, half_height)
+    def Dot(self, vector1, vector2):
+        return vector1.x * vector2.x + vector1.y * vector2.y
+    
+    def RotatePoint(self, point, angle):
+        x_rot = point.x * cos(angle) - point.y * sin(angle)
+        y_rot = point.x * sin(angle) + point.y * cos(angle)
+        return Vector2D(x_rot, y_rot)
+    
+    def GetPoints(self, rect):
+        halfWidth = rect.width/2
+        halfHeight = rect.height/2
+        return [
+            rect.position + self.RotatePoint(Vector2D(halfWidth, halfHeight), radians(rect.rotation)),
+            rect.position - self.RotatePoint(Vector2D(halfWidth, halfHeight), radians(rect.rotation)),
+            rect.position + self.RotatePoint(Vector2D(halfWidth, -halfHeight), radians(rect.rotation)),
+            rect.position - self.RotatePoint(Vector2D(-halfWidth, halfHeight), radians(rect.rotation)),
         ]
-        return [self.edge_direction(vertices[i], vertices[(i + 1) % len(vertices)])
-                for i in range(len(vertices))]
 
-    def project(self, axis):
-        half_width = self.width / 2
-        half_height = self.height / 2
-        vertices = [
-            (-half_width, -half_height),
-            (half_width, -half_height),
-            (half_width, half_height),
-            (-half_width, half_height)
-        ]
-        dots = [self.dot(vertex, axis) for vertex in vertices]
-        return [min(dots), max(dots)]
-
-    def overlap(self, projection1, projection2):
-        return min(projection1) <= max(projection2) and \
-               min(projection2) <= max(projection1)
-
-    def separating_axis_theorem(self, other):
-        edges = self.vertices_to_edges() + other.vertices_to_edges()
-        axes = [self.normalize(self.orthogonal(edge)) for edge in edges]
-
-        for axis in axes:
-            projection_self = self.project(axis)
-            projection_other = other.project(axis)
-
-            overlapping = self.overlap(projection_self, projection_other)
-
-            if not overlapping:
-                return False
+    def CheckCollision(self, other):
+        selfPoints = self.GetPoints(self)
+        otherPoints = self.GetPoints(other)
+        
+        for i in range(2):
+            if i == 1:
+                otherPoints, selfPoints = selfPoints, otherPoints
+            for a in range(len(selfPoints)):
+                b = (a + 1) % len(selfPoints)
+                axisProjection = Vector2D(-(selfPoints[b].y - selfPoints[a].y), selfPoints[b].x - selfPoints[a].x)
+                axisProjection = self.Normalize(axisProjection)
+                
+                minRange1 = sys.maxsize
+                maxRange1 = -sys.maxsize - 1
+                for p in range(len(selfPoints)):
+                    q = self.Dot(Vector2D(selfPoints[p].x, selfPoints[p].y), axisProjection)
+                    minRange1 = min(minRange1, q)
+                    maxRange1 = max(maxRange1, q)
+                    
+                minRange2 = sys.maxsize
+                maxRange2 = -sys.maxsize - 1
+                for p in range(len(otherPoints)):
+                    q = self.Dot(Vector2D(otherPoints[p].x, otherPoints[p].y), axisProjection)
+                    minRange2 = min(minRange2, q)
+                    maxRange2 = max(maxRange2, q)
+                
+                if not (maxRange2 >= minRange1 and maxRange1 >= minRange2):
+                    return False
 
         return True
 
 
 if __name__ == "__main__":
+    a = RectangleCollision(Vector2D(0, 0), 1, 1, 0) # cube of 0.5 from center wide/high
+    b = RectangleCollision(Vector2D(1, 1), 1, 1, 0)
+    print(a.CheckCollision(b))
